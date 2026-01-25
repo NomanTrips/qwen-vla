d9bcc23d0f8d726f4163d6e5cf99e3918bd1356f
Brian Scott <brianscott0017@yahoo.com>
2026-01-25 17:43:45 -0500

    Add Qwen3 VL integration module

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+"""Model components for QwenVLA."""
+
+from .qwen3_vl import Qwen3VLConfig, Qwen3VLFeatureExtractor, load_qwen3_vl
+
+__all__ = ["Qwen3VLConfig", "Qwen3VLFeatureExtractor", "load_qwen3_vl"]
